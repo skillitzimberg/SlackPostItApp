f49f84b74c803e519480bfc74d549ba77d6e22ad
Sam Orozco <sam.orozco@apptreesoftware.com>
2019-03-20 12:40:28 -0700

    moving common types to their own file

--- common_pkg/object_diff.go
+++ common_pkg/object_diff.go
@@ -6,16 +6,6 @@ import (
 	"reflect"
 )
 
-// common types
-type JsonMap map[string]interface{}
-type LookupMap map[string]bool
-
-func (mp LookupMap) contains(val string) bool {
-	if _, ok := mp[val]; ok {
-		return ok
-	}
-	return false
-}
 
 type ObjectDiff struct {
 }
