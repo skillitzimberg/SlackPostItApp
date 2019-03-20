31bb073bccfc73792b720c4f2e4117e28eff9f39
Sam Orozco <sam.orozco@apptreesoftware.com>
2019-03-20 12:39:03 -0700

    adding test for fields to exclude

--- common_pkg/object_diff_test.go
+++ common_pkg/object_diff_test.go
@@ -6,6 +6,7 @@ const testValue0 = `{"Left":{"name":"sam","age":25,"last":"orozco","height":6.1,
 const testValue1 = `{"Left":{"name":"samo","age":25,"last":"orozco","height":6.1,"cousin":{"name":"kacy","age":22},"Teams":["orozco","sco"]},"Right":{"name":"sam","age":25,"last":"orozco","height":6.1,"cousin":{"name":"kacy","age":22},"Teams":["orozco","sco"]},"FieldsToCompare":["name","age","last","height","cousin","Teams"]}`
 const testValue2 = `{"Left":{"name":"samo","age":25,"last":"orozco","height":6.1,"cousin":{"name":"kacyy","age":22},"Teams":["orozco","sco"]},"Right":{"name":"sam","age":25,"last":"orozco","height":6.1,"cousin":{"name":"kacy","age":22},"Teams":["orozco","sco"]},"FieldsToCompare":["name","age","last","height","cousin","Teams"]}`
 const testValue3 = `{"Left":{"name":"sam","age":24,"last":"orozco","height":6.1,"cousin":{"name":"kacy","age":22},"Teams":["orozco","sco"]},"Right":{"name":"sam","age":25,"last":"orozco","height":6.1,"cousin":{"name":"kacy","age":22},"Teams":["orozco","sco"]},"FieldsToCompare":[]}`
+const testValue4 = `{"Left":{"name":"samo","age":25,"last":"orozco","height":6.1,"cousin":{"name":"kacy","age":22},"Teams":["orozco","sco"]},"Right":{"name":"sam","age":25,"last":"orozco","height":6.1,"cousin":{"name":"kacy","age":22},"Teams":["orozco","sco"]},"FieldsToCompare":["name","age","last","height","cousin","Teams"],"FieldsToExclude":["name"]}`
 
 func TestObjectDiff_ExecuteJson(t *testing.T) {
 	diff := ObjectDiff{}
@@ -67,6 +68,18 @@ func TestObjectDiff_ExecuteJson3(t *testing.T) {
 	}
 
 	if val.FieldsThatDiffered[0] != "age" {
+		t.Fail()
+	}
+}
+
+func TestObjectDiff_ExecuteJson4(t *testing.T) {
+	diff := ObjectDiff{}
+	obj, err := diff.ExecuteJson(testValue4)
+	if err != nil {
+		panic(err)
+	}
+	val := obj.(ObjectDiffOutput)
+	if val.Different {
 		t.Fail()
 	}
 }
