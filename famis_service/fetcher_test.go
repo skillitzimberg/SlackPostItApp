2bf5d6666aae9fa14eb0a3756c9944f61e6d4d63
Sam Orozco <sam.orozco@apptreesoftware.com>
2019-03-22 15:05:05 -0700

    first version of the fetcher before I have tested

--- famis_service/fetcher_test.go
+++ famis_service/fetcher_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestFetcher_Execute(t *testing.T) {
 	fetcher := Fetcher{}
-	tok, _, err := fetcher.Login("apptree", "moreAPPS", "https://st-ccsd.accruenttest.net")
+	tok, _, _, err := fetcher.Login("apptree", "moreAPPS", "https://st-ccsd.accruenttest.net")
 	if err != nil {
 		t.Fail()
 		panic(err)
