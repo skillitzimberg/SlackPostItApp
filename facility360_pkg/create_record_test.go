3ea330ae1339065cb0252df2c3fc8bff29d59f0a
Sam Orozco <sam.orozco@apptreesoftware.com>
2019-03-26 11:13:59 -0700

    updating responses

--- facility360_pkg/create_record_test.go
+++ facility360_pkg/create_record_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 const testJson1 = `{"Username":"<username>","Password":"<password>","Url":"https://st-ccsd.accruenttest.net","Endpoint":"/MobileWebServices/apis/360facility/v1/assets","Record":{"Name":"NY3-0110BMS -01-BMS","SerialNumber":"46171613","StatusComment":"Detection Method: Semiconductor\r\nPower Source: 120 VAC\r\nAudible Alert: 85dB @ 10ft\r\nOperating Temp: -40 to 150 F\r\n","BarcodeNumber":"1","SpaceId":598,"ActiveFlag":false,"MakeId":413,"ModelId":1033,"AssetStatusId":1,"AssetRankId":3,"AssetClassId":107,"AssetNumber":"3331"}}`
-const testJson2 = `{"Username":"<username>","<password>":"5PtHAbMsU4sALRU","Url":"https://st-ccsd.accruenttest.net","Endpoint":"/MobileWebServices/apis/360facility/v1/assets","Record":{"SerialNumber":"46171613","StatusComment":"Detection Method: Semiconductor\r\nPower Source: 120 VAC\r\nAudible Alert: 85dB @ 10ft\r\nOperating Temp: -40 to 150 F\r\n"},"Id":8799}`
+const testJson2 = `{"Username":"<username>","Password":"<password>","Url":"https://st-ccsd.accruenttest.net","Endpoint":"/MobileWebServices/apis/360facility/v1/assets","Record":{"SerialNumber":"46171613","StatusComment":"Detection Method: Semiconductor\r\nPower Source: 120 VAC\r\nAudible Alert: 85dB @ 10ft\r\nOperating Temp: -40 to 150 F\r\n"},"Id":8799}`
 
 func TestCreateRecordJson(test *testing.T) {
 	create := CreateRecord{}
