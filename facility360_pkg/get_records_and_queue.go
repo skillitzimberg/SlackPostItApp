a6e9675ef5690e1e45d813c922b136ed27db0fe1
Alexis Lasher <alexis.lasher@apptreesoftware.com>
2019-04-02 10:06:40 -0700

    added child path to fetch and queue method so that child items can be queued to a workflow instead of the parent item

--- facility360_pkg/get_records_and_queue.go
+++ facility360_pkg/get_records_and_queue.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/apptreesoftware/go-workflow/pkg/step"
-	jsoniter "github.com/json-iterator/go"
+	"github.com/json-iterator/go"
 	"log"
 )
 
@@ -47,11 +47,47 @@ func (fetch FetchAndQueue) executeQueue(input FetchAndQueueInput, in step.Contex
 	engine := in.Engine()
 	err = fetch.performPagedFetch(uri, input.Endpoint, func(messages []jsoniter.RawMessage) {
 		for _, msg := range messages {
-			err := engine.AddToQueue(input.Workflow, msg)
-			if err != nil {
-				log.Fatalf("Unable to add item to engine queue: %s", err.Error())
+			if input.ChildPath == "" {
+				err := engine.AddToQueue(input.Workflow, msg)
+				if err != nil {
+					log.Fatalf("Unable to add item to engine queue: %s", err.Error())
+				}
+				continue
 			}
+			parseAndQueueChildItems(msg, input, engine)
 		}
 	})
 	return nil, err
+}
+
+/**
+This method will attempt to parse the raw json into a map and then look for the record at the input path.
+If it is an array, then it will loop through the array and queue each item separately, otherwise it queues
+whatever it finds at the path.
+ */
+func parseAndQueueChildItems(msg jsoniter.RawMessage, input FetchAndQueueInput, engine step.Engine) {
+	var record map[string]interface{}
+	err := jsoniter.Unmarshal(msg, &record)
+	if err != nil {
+		log.Fatalf("Unable to parse item from response: %s", err.Error())
+		return
+	}
+	children := record[input.ChildPath]
+	if children == nil {
+		return
+	}
+	if arrayVal, ok := children.([]interface{}); ok {
+		for _, record := range arrayVal {
+			err := engine.AddToQueue(input.Workflow, record)
+			if err != nil {
+				log.Fatalf("Unable to add item to engine queue: %s", err.Error())					}
+		}
+		return
+	}
+	if singleVal, ok := children.(interface{}); ok {
+		err := engine.AddToQueue(input.Workflow, singleVal)
+		if err != nil {
+			log.Fatalf("Unable to add item to engine queue: %s", err.Error())
+		}
+	}
 }
