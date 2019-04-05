907b5c88f90402367ab45694783328d8ee594355
Alexis Lasher <alexis.lasher@apptreesoftware.com>
2019-04-05 13:00:10 -0700

    no message

--- database/firebase_pkg/main.go
+++ database/firebase_pkg/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "github.com/apptreesoftware/go-workflow/pkg/step"
 
+func main() {
+	step.Register(Query{})
+	step.Register(QueryAndQueue{})
+	step.Register(UpdateDocument{})
+	step.Register(Upsert{})
+	step.Run()
 }
